48cd8dfc083026f61f5a576289137e174bd5d5ac
Khandakar Aryanul Haque <aryanul2004@gmail.com>
2025-04-06 19:53:18 +0530

    Nav-Bar added

---
+++ project-1/src/components/Navigation.jsx
@@ -0,0 +1,19 @@
+const Naviagtion = () => {
+  return (
+    <nav>
+          <a href="/">
+            <img src="/images/brand_logo.png" alt="" />
+          </a>
+          <ul>
+            <li href="/">MENU</li>
+            <li href="/">LOCATION</li>
+            <li href="/">ABOUT</li>
+            <li href="/">CONTACT</li>
+             
+          </ul>
+          <button className="login_button">Login</button>
+        </nav>
+  );
+}
+
+export default Naviagtion;
